b3dca8094ce376760d3a7b2f9954058d9d80b69b
chrispiccaro <chrispiccaro18@gmail.com>
2020-12-15 11:51:34 -0800

    basic conversions

---
+++ main.js
@@ -0,0 +1,24 @@
+const CONSTANT = 2.2046;
+
+
+const kgToLb = kg => (kg * CONSTANT).toFixed(6);
+const lbToKg = lb => (lb / CONSTANT).toFixed(6);
+const pricePerKgToLb = pricePerKg => (pricePerKg / CONSTANT).toFixed(6);
+const pricePerLbToKg = pricePerLb => (pricePerLb * CONSTANT).toFixed(6);
+
+const functions = {
+  kgToLb,
+  lbToKg,
+  pricePerKgToLb,
+  pricePerLbToKg,
+};
+
+const inputs = document.querySelectorAll('input');
+
+inputs.forEach(input => {
+  input.addEventListener("input", () => {
+    const result = functions[input.id](input.value);
+    const resultDisplay = document.getElementById(`${input.id}Result`);
+    resultDisplay.textContent = result;
+  })
+});
